e7be224c191f61d792cab321124943874eaa4132
Lory Michelle Bresciani Miristice <brescianilory@gmail.com>
2022-09-15 14:42:06 +0200

    aggregate matrices

--- AggregateZonesWithDemand/Solver.cs
+++ AggregateZonesWithDemand/Solver.cs
@@ -9,33 +9,44 @@ namespace AggregateZonesWithDemand
         {
             HashSet<string> importedZones = new();
 
-            var files = Directory.GetFiles(InputFileFolder).Where(file => Path.GetExtension(file) == ".att");
+            var files = Directory.GetFiles(InputFileFolder).Where(file => Path.GetExtension(file) == ".mtx");
             foreach (var file in files)
             {
                 using var fileStream = File.OpenRead(file);
                 using var streamReader = new StreamReader(fileStream, Encoding.UTF8);
                 var counter = 0;
                 var line = streamReader.ReadLine();
-                while (counter < 10 && !line.StartsWith("$ZONE:"))
+                while (counter < 5 && !line.StartsWith("$"))
                 {
                     line = streamReader.ReadLine();
                     if (string.IsNullOrEmpty(line)) counter++;
                 }
                 if (string.IsNullOrEmpty(line)) break;
 
-                line = line.Replace("$ZONE:", string.Empty);
+                line = line.Replace("$", string.Empty);
                 var headersArray = line.Split(';');
                 var headers = Enumerable.Range(0, headersArray.Length).ToDictionary(x => headersArray[x]);
-                if (!headers.ContainsKey("NO")) break;
+                if (!headers.ContainsKey("O")) break;
 
-                var noIndex = headers["NO"];
+
+                line = streamReader.ReadLine();
+                while (counter < 4 || !line.StartsWith("*"))
+                {
+                    line = streamReader.ReadLine();
+                    counter++;
+                }
+
+                var counter2 = 0;
+                int oIndex = headers["O"];
+                int dIndex = headers["D3"];
                 while (true)
                 {
                     line = streamReader.ReadLine();
-                    if (string.IsNullOrEmpty(line)) break;
-
-                    var fields = line.Split(';');
-                    importedZones.Add(fields[noIndex]);
+                    if (string.IsNullOrEmpty(line) || line.StartsWith("*")) break;
+                    counter2++;
+                    var fields = line.Split(' ');
+                    importedZones.Add(fields[oIndex]);
+                    importedZones.Add(fields[dIndex]);
                 }
             }
             return importedZones;
