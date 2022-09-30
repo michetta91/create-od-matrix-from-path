465829e5a4b3d4121d063985bf2b4707f45aa610
Lory Michelle Bresciani Miristice <brescianilory@gmail.com>
2022-09-30 18:47:15 +0200

    prova
    pensaci meglio, probabilmente il timeprofile è giusto il problema è che dovresti leggere destinazione e origine

---
+++ CreateMainZonesAccordingToStopPoint/ConnectionChecker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateMainZonesAccordingToStopPoint
+{
+    internal class ConnectionChecker
+    {
+        private HashSet<int> TransitStopToKeep { get; set; }
+        internal int CurrentZoneNumber { get; private set; }
+        internal int PreviousStopPointNumber { get; private set; }
+        internal ConnectionChecker(HashSet<int> transitStopToKeep)
+        {
+            TransitStopToKeep = transitStopToKeep;
+        }
+
+        internal void Init(int newOriginZoneNumber)
+        {
+            CurrentZoneNumber = newOriginZoneNumber;
+            PreviousStopPointNumber = 0;
+        }
+
+        internal bool ShouldSaveStop(int currentStopPointNumber)
+        {
+            if (PreviousStopPointNumber != currentStopPointNumber &&
+                TransitStopToKeep.Contains(currentStopPointNumber))
+            {
+                PreviousStopPointNumber = currentStopPointNumber;
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- CreateMainZonesAccordingToStopPoint/Solver.cs
+++ CreateMainZonesAccordingToStopPoint/Solver.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text;
+﻿using System.Diagnostics;
 
 namespace CreateMainZonesAccordingToStopPoint
 {
@@ -16,72 +15,53 @@ namespace CreateMainZonesAccordingToStopPoint
 
         private static List<ElementToMerge> ImportPathLegs(string inputFolder, HashSet<int> transitStopToKeep)
         {
+
             Dictionary<int, ElementToMerge> elementsToMerge = new();
             var files = Directory.GetFiles(inputFolder).Where(file => Path.GetExtension(file) == ".att");
             foreach (var file in files)
             {
-                using var fileStream = File.OpenRead(file);
-                using var streamReader = new StreamReader(fileStream, Encoding.UTF8);
-                var counter = 0;
-                var line = streamReader.ReadLine();
-                while (!line.StartsWith("$PUTPATHLEG:"))
-                {
-                    line = streamReader.ReadLine();
-                    if (string.IsNullOrEmpty(line)) counter++;
-                }
-                if (string.IsNullOrEmpty(line)) break;
-
-                line = line.Replace("$PUTPATHLEG:", string.Empty);
-                var headersArray = line.Split(';');
-                var headers = Enumerable.Range(0, headersArray.Length).ToDictionary(x => headersArray[x]);
-                if (!headers.ContainsKey("ORIGZONENO")) break;
+                var result = ComputeHeaders(file);
+                if (result is null) continue;
 
 
-                int originZoneNumberIndex = headers["ORIGZONENO"];
-                int fromStopPointNoIndex = headers["FROMSTOPPOINTNO"];
-                int currentOriginZoneNumber = 0;
-                int previousStopPointNumber = 0;
+                var linesOrigin = File.ReadAllLines(file).Skip(result.Value.headerLine).ToArray();
+                int originZoneNumberIndex = result.Value.headers["ORIGZONENO"];
+                int fromStopPointNoIndex = result.Value.headers["FROMSTOPPOINTNO"];
+                elementsToMerge.ImportStops(transitStopToKeep, linesOrigin, originZoneNumberIndex, fromStopPointNoIndex, fromOrigin: true);
 
-                while (true)
-                {
-                    line = streamReader.ReadLine();
-                    if (string.IsNullOrEmpty(line)) break;
-                    var fields = line.Split(';');
-
-                    var originZoneNumber = fields[originZoneNumberIndex];
-                    if (!string.IsNullOrEmpty(originZoneNumber))
-                    {
-                        var newOriginZoneNumber = int.Parse(originZoneNumber);
-                        currentOriginZoneNumber = newOriginZoneNumber;
-                        previousStopPointNumber = 0;
-                    }
-
-                    var connectedStopPoint = fields[fromStopPointNoIndex];
-                    if (string.IsNullOrEmpty(connectedStopPoint)) continue;
-                    if (!elementsToMerge.ContainsKey(currentOriginZoneNumber))
-                    {
-                        elementsToMerge.Add(currentOriginZoneNumber, new ElementToMerge(currentOriginZoneNumber));
-                    }
-
-                    if (previousStopPointNumber != 0) continue;
-
-
-                    var currentStopPointNumber = int.Parse(connectedStopPoint);
-
-                    if (previousStopPointNumber != currentStopPointNumber && transitStopToKeep.Contains(currentStopPointNumber))
-                    {
-                        elementsToMerge[currentOriginZoneNumber].AddConnectedTransitStop(int.Parse(connectedStopPoint));
-                        previousStopPointNumber = currentStopPointNumber;
-                    }
-                }
+                var linesDestination = linesOrigin.Reverse().ToArray();
+                int destinationZoneNumberIndex = result.Value.headers["DESTZONENO"];
+                int toStopPointNoIndex = result.Value.headers["TOSTOPPOINTNO"];
+                elementsToMerge.ImportStops(transitStopToKeep, linesDestination, destinationZoneNumberIndex, toStopPointNoIndex, fromOrigin: false);
             }
 
             return elementsToMerge.Values.ToList();
         }
 
+        private static (int headerLine, Dictionary<string, int> headers)? ComputeHeaders(string file)
+        {
+            var lines = File.ReadAllLines(file);
+            int counter = 0;
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("$PUTPATHLEG:"))
+                {
+                    var headersArray = line.Replace("$PUTPATHLEG:", "").Split(';');
+                    var headers = Enumerable.Range(0, headersArray.Length).ToDictionary(x => headersArray[x]);
+                    if (headers is null ||
+                        !headers.ContainsKey("ORIGZONENO") || 
+                        !headers.ContainsKey("DESTZONENO")) return null;
+
+                    return (counter + 1, headers);
+
+                }
+                counter++;
+            }
+            return null;
+        }
+
         private static Maps MergeElements(List<ElementToMerge> elementsToMerge)
         {
-
             var mergedElements = new Dictionary<string, MergedElements>();
             foreach (var element in elementsToMerge)
             {
@@ -89,6 +69,7 @@ namespace CreateMainZonesAccordingToStopPoint
 
                 if (!mergedElements.ContainsKey(element.Key))
                 {
+                    if (element.Key == "1388") Debugger.Break();
                     mergedElements.Add(element.Key,
                                        new MergedElements(element.ConnectedTransitStopNumbers));
                 }
@@ -107,6 +88,7 @@ namespace CreateMainZonesAccordingToStopPoint
                 mainZoneToElementMap.Add(counter, mergedElement.Key);
                 foreach (var connectedZoneNumber in mergedElement.ConnectedZoneNumbers)
                 {
+                    if (connectedZoneNumber == 0) Debugger.Break();
                     zoneToMainZoneMap.Add(connectedZoneNumber, counter);
                 }
 
